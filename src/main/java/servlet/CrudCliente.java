645c9c50151b6285f517e9115dfb983543570160
Miller B <alvarezsalasangelica8@gmail.com>
2025-05-25 21:17:32 -0500

    Funcionalidad de actualizar datos

--- src/main/java/servlet/CrudCliente.java
+++ src/main/java/servlet/CrudCliente.java
@@ -2,11 +2,14 @@ package servlet;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import dao.ClienteJpaController;
 import dto.Cliente;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,7 +37,7 @@ public class CrudCliente extends HttpServlet {
                 ClienteJpaController cjc = new ClienteJpaController();
                 List<Cliente> clientes = cjc.findClienteEntities();
                 jsonResponse.addProperty("action", "show");
-                jsonResponse.add("clientes", gson.toJsonTree(clientes));
+                jsonResponse.add("data", gson.toJsonTree(clientes));
             } else {
                 jsonResponse.addProperty("message", "El token expiró o es invalido");
                 jsonResponse.addProperty("action", "update");
@@ -46,6 +49,39 @@ public class CrudCliente extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            BufferedReader reader = request.getReader();
+            String json = reader.lines().collect(Collectors.joining());
+            JsonObject obj = JsonParser.parseString(json).getAsJsonObject();
+
+            String codigo = obj.get("codigo").getAsString();
+            String nombre = obj.get("nombre").getAsString();
+            String apPaterno = obj.get("appater").getAsString();
+            String apMaterno = obj.get("apmater").getAsString();
+            ClienteJpaController cjc = new ClienteJpaController();
+            Cliente cliente = cjc.findCliente(codigo);
+            Gson gson = new Gson();
+            JsonObject jsonObject = new JsonObject();
+            try {
+                if (cliente != null) {
+                    cliente.setNombClie(nombre);
+                    cliente.setAppaClie(apPaterno);
+                    cliente.setApmaClie(apMaterno);
+                    cjc.edit(cliente);
+                    jsonObject.addProperty("result", "updated");
+                }
+            } catch (Exception e) {
+                jsonObject.addProperty("result", "error" + e);
+                e.printStackTrace();
+            }
+            out.print(gson.toJson(jsonObject));
+        }
     }
 
     @Override
--- src/main/webapp/dist/js/tables.js
+++ src/main/webapp/dist/js/tables.js
@@ -1,31 +1,91 @@
 document.addEventListener("DOMContentLoaded", async () => {
     let token = getCookie("token");
-    const response = await fetch("/Examen2Prac/crud-clientes", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + token
-        }
-    }).then(response => response.json());
-
-    if (response.action === "show") {
-        console.log(response);
-        const data = response.clientes;
-        const tbody = document.getElementById("dataTable");
-        data.forEach(p => {
-            const fila = `
-                <tr class="align-middle">
-                    <td>${p.codiClie}</td>
-                    <td>${p.appaClie}</td>
-                    <td>${p.apmaClie}</td>
-                    <td>${p.nombClie}</td>
-                </tr>`;
-            tbody.innerHTML += fila;
-        });
+    const modalEditar = new bootstrap.Modal(document.getElementById("modalEditar"));
+    const btnEditar = document.getElementById("btnEditar");
+    if (token) {
+        await fetchData();
     } else {
-        alert("Debe iniciar sesión");
+        alert("Debe iniciar sesión.");
         window.location.href = "../../../index.html";
     }
+    async function fetchData() {
+        const response = await fetch("/Examen2Prac/crud-clientes", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            }
+        }).then(response => response.json());
+
+        if (response.action === "show") {
+            console.log(response);
+            const data = response.data;
+            const tbody = document.getElementById("dataTable");
+            tbody.innerHTML = ""; // Clear existing rows before rendering new ones
+            data.forEach(cliente => {
+                const row = renderFila(cliente);
+                tbody.appendChild(row);
+            });
+        } else {
+            alert("Debe iniciar sesión");
+            window.location.href = "../../../index.html";
+        }
+    }
+
+    function renderFila(p) {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+        <td>${p.codiClie}</td>
+        <td>${p.nombClie}</td>
+        <td>${p.appaClie}</td>
+        <td>${p.apmaClie}</td>
+        <td class="text-center">
+            <button class="btn btn-sm btn-primary editar-btn">Editar</button>
+        </td>
+        <td class="text-center">
+            <button class="btn btn-sm btn-danger eliminar-btn">Eliminar</button>
+        </td>
+    `;
+        fila.querySelector(".editar-btn").addEventListener("click", () => {
+            abrirModalEditar(p);
+        });
+        fila.querySelector(".eliminar-btn").addEventListener("click", () => {
+            abrirModalEditar(p);
+        });
+        return fila;
+    }
+
+    function abrirModalEditar(p) {
+        document.getElementById("upCodigo").value = p.codiClie;
+        document.getElementById("upAppaterno").value = p.appaClie;
+        document.getElementById("upApmaterno").value = p.apmaClie;
+        document.getElementById("upNombre").value = p.nombClie;
+        modalEditar.show();
+    }
+
+    btnEditar.addEventListener("click", async () => {
+        const codigo = document.getElementById("upCodigo").value;
+        const appater = document.getElementById("upAppaterno").value;
+        const apmater = document.getElementById("upApmaterno").value;
+        const  nombre = document.getElementById("upNombre").value;
+        const data = {codigo: codigo, appater: appater, apmater: apmater, nombre: nombre};
+        const response = await fetch("/Examen2Prac/crud-clientes", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json());
+        if (response.result === "updated") {
+            alert("Acutalizado correctamente");
+            fetchData();
+            modalEditar.hide();
+        } else {
+            alter("Error al actualizar");
+            console.log(response.result);
+        }
+    });
 
     function getCookie(nombre) {
         const valor = `; ${document.cookie}`;
